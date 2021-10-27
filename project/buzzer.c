b7b93f45d3eec901bb99b6bd834fb6f6a024daea
Li1Davey <dasanchez13@miners.utep.edu>
2021-10-27 10:56:14 -0600

    File that sets the frequency of the buzzer

--- project/buzzer.c
+++ project/buzzer.c
@@ -23,3 +23,5 @@ void buzzer_set_period(short cycles) /* buzzer clock = 2MHz.  (period of 1k resu
   CCR0 = cycles; 
   CCR1 = cycles >> 1;		/* one half cycle */
 }
+
+/* remained unchanged from the original */
