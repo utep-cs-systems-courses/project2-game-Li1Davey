7bd1a47a692c970dd66a847e6cb0fa27a25c0327
Li1Davey <dasanchez13@miners.utep.edu>
2021-10-27 11:09:13 -0600

    Uses the C programming language to turn the LEDs off or on

--- project/lightControl-c.c
+++ project/lightControl-c.c
@@ -3,18 +3,18 @@
 
 void lightControl(int trig){
   switch(trig){
-  case 0://Both off
+  case 0://Both LEDs turned off
     P1OUT &= ~LEDS;
     break;
-  case 1://Green on only
+  case 1://Green LED turned on only
     P1OUT |= LED_GREEN;
     P1OUT &= ~LED_RED;
     break;
-  case 2://Red on only
+  case 2://Red LED turned on only
     P1OUT &= ~LED_GREEN;
     P1OUT |= LED_RED;
     break;
-  default://Both on
+  default://Both LEDs turned on
     P1OUT |= LEDS;
     break;
   }
