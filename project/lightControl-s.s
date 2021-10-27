b06e76adce78ef656f0fb2fc308f2c903e60e482
Li1Davey <dasanchez13@miners.utep.edu>
2021-10-27 11:12:09 -0600

    Uses the assembly language to turn the LEDs off or on

--- project/lightControl-s.s
+++ project/lightControl-s.s
@@ -6,19 +6,19 @@
 	.extern P1OUT
 
 lightControl:
-	cmp #0, r12 		;Both Lights Off
+	cmp #0, r12 		;Both LEDs Off
 	jne case_1
 	and #~65, &P1OUT
 	pop r0
-case_1:	cmp #1, r12 		;Green Light On Only
+case_1:	cmp #1, r12 		;Green LED On Only
 	jne case_2
 	bis #64, &P1OUT
 	and #~1, &P1OUT
 	pop r0
-case_2: cmp #2, r12 		;Red Light On Only
+case_2: cmp #2, r12 		;Red LED On Only
 	jne def
 	and #~64, &P1OUT
 	bis #1, &P1OUT
 	pop r0
-def:	bis #65, &P1OUT 	;Both Lights On
+def:	bis #65, &P1OUT 	;Both LEDs On
 	pop r0
